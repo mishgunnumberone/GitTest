06b1d147ef90455c5ee5065f72ffc754ca90b01e
Mikhail <omibo006@edu.xamk.fi>
2018-01-18 21:09:28 +0200

    Revert "Add new person object"
    This reverts commit 97be13e18fb3c8a1b5c1f15b24295bbac605b810.

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,10 +7,13 @@ namespace GitTest
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+<<<<<<< HEAD
 
 
             Person person1 = new Person("Daniel", 19);
 
+=======
+>>>>>>> parent of 97be13e... Add new person object
         }
     }
 }
