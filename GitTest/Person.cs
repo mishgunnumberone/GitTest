31216acf96da63b9710d91bd9c8c596fe8980f95
Mikhail <omibo006@edu.xamk.fi>
2018-01-18 20:49:43 +0200

    Add Person class

---
+++ GitTest/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace GitTest
+{
+    public class Person
+    {
+        string Name;
+        int Age;
+
+        public Person(string Name, int Age)
+        {
+            this.Name = Name;
+            this.Age = Age;
+        }
+    }
+}
