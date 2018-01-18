819b1fb9a063397e3658705371cfa05b6603454e
Mikhail <omibo006@edu.xamk.fi>
2018-01-18 21:23:01 +0200

    Revert "Add new person object"

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -7,13 +7,10 @@ namespace GitTest
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-<<<<<<< HEAD
 
 
             Person person1 = new Person("Daniel", 19);
 
-=======
->>>>>>> parent of 97be13e... Add new person object
         }
     }
 }
